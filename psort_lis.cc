88ae7e32c4d5e0dd7ebd5722145d4fc5162f4cea
Saad Taame <saadtaame@users.noreply.github.com>
2017-03-12 12:23:56 +0000

    Update psort_lis.cc

--- psort_lis.cc
+++ psort_lis.cc
@@ -1,70 +1,32 @@
-#include <cstdio>
-#include <stack>
-#include <vector>
-#include <iostream>
-#include <algorithm>
+
+#include <bits/stdc++.h>
 
 using namespace std;
 
-bool pCmp( const stack<int> &S, int val ) {
-    return (S.top() < val);
+void add(vector<int> &piles, int a) {
+    auto it = lower_bound(piles.begin(), piles.end(), a);
+    if(it == piles.end()) piles.push_back(a);
+    else piles[ distance(piles.begin(), it) ] = a;
 }
 
-void printSequence( vector<int> &prec, int pos ) {
-    if(prec[pos] != -1) {
-        printSequence(prec, prec[pos]);
-        cout << " " << pos;
-    }
-    else {
-        cout << pos;
-    }
+int LIS(vector<int> A) {
+    int n = (int)A.size();
+    vector<int> piles; // Top elements of piles
+    
+    for(int i = 0; i < n; i++)
+        add(piles, A[i]);
+        
+    return (int)piles.size();
 }
 
-int main( void ) {
+int main(void) {
 
-    int n;
+	int n;
+	cin >> n;
+	vector<int> A(n);
+	for(int i = 0; i < n; i++)
+		cin >> A[i];
+	cout << LIS(A) << endl;
 
-    while(cin >> n) {
-
-        vector<int> prec(n, -1);
-        vector< stack<int> > piles, positions;
-        int x, curPos;
-
-        curPos = 0;
-
-        while(n--) {
-
-            cin >> x;
-
-            auto it = lower_bound(piles.begin(), piles.end(), x, pCmp);
-            int pos;
-
-            if(it == piles.end()) {
-                stack<int> newPile;
-                newPile.push(x);
-                piles.push_back(newPile);
-
-                stack<int> dummy;
-                dummy.push(curPos);
-                positions.push_back(dummy);
-
-                pos = (int)piles.size() - 1;
-            }
-            else {
-                it->push(x);
-                pos = distance(piles.begin(), it);
-                positions[pos].push(curPos);
-            }
-
-            if(pos > 0) {
-                prec[curPos] = positions[pos - 1].top();
-            }
-            curPos += 1;
-        }
-
-        cout << (int)piles.size() << endl;
-        printSequence(prec, positions.back().top());
-        cout << endl;
-    }
-    return 0;
+	return 0;
 }
